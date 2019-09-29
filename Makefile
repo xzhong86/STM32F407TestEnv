33b0d50cfe993314b824a333d21cf2b53f738478
BruceZ <xzhong86@163.com>
2019-09-30 00:01:05 +0800

    init repo, pass compilation

---
+++ Makefile
@@ -0,0 +1,86 @@
+# zpzhong, stm32 simple project
+
+PREFIX = $(HOME)/local/bin/arm-none-eabi-
+
+ST_FLASH = $(HOME)/local/bin/st-flash
+
+# stm32f103c8t6
+#MCU = STM32F103xB
+# stm32f401ccub
+#MCU = STM32F401xC
+# stm32f407vet6
+MCU = STM32F407xx
+
+CUBE_ROOT    = $(HOME)/work/geek/stm32/stm32cube/STM32Cube_FW_F4_V1.24.0
+CUBE_DRIVER  = $(CUBE_ROOT)/Drivers
+CUBE_PROJECT = $(CUBE_ROOT)/Projects/STM32F4-Discovery/Templates
+STM32_TYPE   = STM32F4xx
+#startup.s = $(CUBE_PROJECT)/SW4STM32/startup_stm32f401xc.s
+#system.c  = $(CUBE_PROJECT)/Src/system_stm32f4xx.c
+startup.s = $(CUBE_ROOT)/Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/gcc/startup_stm32f407xx.s
+system.c  = $(CUBE_ROOT)/Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c
+ld-script = $(CUBE_PROJECT)/SW4STM32/STM32F4-Discovery/STM32F407VGTx_FLASH.ld
+
+hal_src   = $(CUBE_DRIVER)/$(STM32_TYPE)_HAL_Driver/Src
+hal_objs  = $(patsubst $(hal_src)/%.c,hal_objs/%.o,$(wildcard $(hal_src)/*.c))
+hal_objs := $(filter-out %template.o, $(hal_objs))
+libhal    = hal_objs/libhal.a
+
+CUBE_INC  = -I$(CUBE_DRIVER)/$(STM32_TYPE)_HAL_Driver/Inc
+CUBE_INC += -I$(CUBE_DRIVER)/CMSIS/Device/ST/$(STM32_TYPE)/Include
+CUBE_INC += -I$(CUBE_DRIVER)/CMSIS/Include
+CUBE_INC += -I$(CUBE_PROJECT)/Inc
+
+com_objs = objs/startup.o objs/system.o
+#src_objs = $(patsubst src/%.c,objs/%.o,$(wildcard src/*.c))
+src_objs = objs/main.o objs/syscalls.o
+
+#CFLAGS += -mcpu=cortex-m3 -mthumb -D$(MCU)
+CFLAGS += -mcpu=cortex-m4 -mthumb -D$(MCU)
+CFLAGS += $(CUBE_INC) -I./src
+CFLAGS += -Os -O2 -g
+
+BIN = gpio.bin
+
+all: _mkdir gpio.elf
+
+objs/startup.o : $(startup.s)
+	$(PREFIX)gcc $(CFLAGS) -c -o $@ $<
+objs/system.o : $(system.c)
+	$(PREFIX)gcc $(CFLAGS) -c -o $@ $<
+
+gpio.elf : $(com_objs) $(src_objs) $(libhal)
+	$(PREFIX)gcc $(CFLAGS) -T $(ld-script) -o $@ $^
+
+$(libhal) : $(hal_objs)
+	$(PREFIX)ar -r $@ $^
+
+hal_objs/%.o : $(hal_src)/%.c
+	$(PREFIX)gcc $(CFLAGS) -c -o $@ $<
+
+objs/%.o : src/%.c
+	$(PREFIX)gcc $(CFLAGS) -c -o $@ $<
+
+%.dump : %.elf
+	$(PREFIX)objdump -D $< > $@
+
+%.bin : %.elf
+	$(PREFIX)objcopy -O binary $< $@
+
+.PHONY: clean flash link
+clean:
+	rm -rf objs/ hal_objs/ *.elf *.bin *.dump
+_mkdir:
+	mkdir -p objs hal_objs
+
+flash: $(BIN)
+	$(ST_FLASH) write $(BIN) 0x8000000
+
+link:   # to view source code
+	ln -s $(STM32F1_CUBE)/Drivers/STM32F1xx_HAL_Driver HAL_Driver
+	ln -s $(STM32F1_CUBE)/Drivers/CMSIS CMSIS
+
+# start gdb server: st-util -m
+# start gdb: arm-none-eabi-gdb gpio.elf
+# connect:   (gdb) target ext localhost:4242
+
---
+++ src/main.c
@@ -0,0 +1,255 @@
+/**
+  ******************************************************************************
+  * @file    GPIO/GPIO_IOToggle/Src/main.c
+  * @author  MCD Application Team
+  * @brief   This example describes how to configure and use GPIOs through
+  *          the STM32F1xx HAL API.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2016 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+
+#ifdef STM32F103xB
+# include "stm32f1xx_hal.h"
+#endif
+#if defined STM32F401xC || defined STM32F407xx
+# define USE_HAL_DRIVER
+# include "stm32f4xx.h"
+//# include "stm32f4xx_hal.h"
+#endif
+ 
+#define LED2_PIN                         GPIO_PIN_13
+#define LED2_GPIO_PORT                   GPIOC
+#define LED2_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOC_CLK_ENABLE()  
+#define LED2_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOC_CLK_DISABLE()  
+
+#define LEDx_GPIO_CLK_ENABLE(__INDEX__)   do { if((__INDEX__) == 0) LED2_GPIO_CLK_ENABLE();} while(0)
+#define LEDx_GPIO_CLK_DISABLE(__INDEX__)  (((__INDEX__) == 0) ? LED2_GPIO_CLK_DISABLE() : 0)
+
+
+/** @addtogroup STM32F1xx_HAL_Examples
+  * @{
+  */
+
+/** @addtogroup GPIO_IOToggle
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+static GPIO_InitTypeDef  GPIO_InitStruct;
+
+/* Private function prototypes -----------------------------------------------*/
+static void SystemClock_Config(void);
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  /* This sample code shows how to use GPIO HAL API to toggle LED2 IO
+    in an infinite loop. */
+
+  /* STM32F103xB HAL library initialization:
+       - Configure the Flash prefetch
+       - Systick timer is configured by default as source of time base, but user 
+         can eventually implement his proper time base source (a general purpose 
+         timer for example or other time source), keeping in mind that Time base 
+         duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and 
+         handled in milliseconds basis.
+       - Set NVIC Group Priority to 4
+       - Low Level Initialization
+     */
+  HAL_Init();
+
+  /* Configure the system clock to 64 MHz */
+  //SystemClock_Config();
+  
+  /* -1- Enable GPIO Clock (to be able to program the configuration registers) */
+  LED2_GPIO_CLK_ENABLE();
+
+  /* -2- Configure IO in output push-pull mode to drive external LEDs */
+  GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull  = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+  GPIO_InitStruct.Pin = LED2_PIN;
+  HAL_GPIO_Init(LED2_GPIO_PORT, &GPIO_InitStruct);
+
+  /* -3- Toggle IO in an infinite loop */
+  while (1)
+  {
+    HAL_GPIO_TogglePin(LED2_GPIO_PORT, LED2_PIN);
+    /* Insert delay 100 ms */
+    HAL_Delay(100);
+  }
+}
+
+
+#ifdef STM32F103xB
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (HSI)
+  *            SYSCLK(Hz)                     = 64000000
+  *            HCLK(Hz)                       = 64000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 2
+  *            APB2 Prescaler                 = 1
+  *            PLLMUL                         = 16
+  *            Flash Latency(WS)              = 2
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef clkinitstruct = {0};
+  RCC_OscInitTypeDef oscinitstruct = {0};
+  
+  /* Configure PLL ------------------------------------------------------*/
+  /* PLL configuration: PLLCLK = (HSI / 2) * PLLMUL = (8 / 2) * 16 = 64 MHz */
+  /* PREDIV1 configuration: PREDIV1CLK = PLLCLK / HSEPredivValue = 64 / 1 = 64 MHz */
+  /* Enable HSI and activate PLL with HSi_DIV2 as source */
+  oscinitstruct.OscillatorType  = RCC_OSCILLATORTYPE_HSI;
+  oscinitstruct.HSEState        = RCC_HSE_OFF;
+  oscinitstruct.LSEState        = RCC_LSE_OFF;
+  oscinitstruct.HSIState        = RCC_HSI_ON;
+  oscinitstruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  oscinitstruct.HSEPredivValue    = RCC_HSE_PREDIV_DIV1;
+  oscinitstruct.PLL.PLLState    = RCC_PLL_ON;
+  oscinitstruct.PLL.PLLSource   = RCC_PLLSOURCE_HSI_DIV2;
+  oscinitstruct.PLL.PLLMUL      = RCC_PLL_MUL16;
+  if (HAL_RCC_OscConfig(&oscinitstruct)!= HAL_OK)
+  {
+    /* Initialization Error */
+    while(1); 
+  }
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  clkinitstruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  clkinitstruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  clkinitstruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  clkinitstruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  clkinitstruct.APB1CLKDivider = RCC_HCLK_DIV2;  
+  if (HAL_RCC_ClockConfig(&clkinitstruct, FLASH_LATENCY_2)!= HAL_OK)
+  {
+    /* Initialization Error */
+    while(1); 
+  }
+}
+#endif
+
+
+#ifdef STM32F401xC
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 84000000
+  *            HCLK(Hz)                       = 84000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 2
+  *            APB2 Prescaler                 = 1
+  *            HSE Frequency(Hz)              = 8000000
+  *            PLL_M                          = 8
+  *            PLL_N                          = 336
+  *            PLL_P                          = 4
+  *            PLL_Q                          = 7
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale2 mode
+  *            Flash Latency(WS)              = 2
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  
+  /* The voltage scaling allows optimizing the power consumption when the device is 
+     clocked below the maximum system frequency, to update the voltage scaling value 
+     regarding system frequency refer to product datasheet.  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    /* Initialization Error */
+    while(1); 
+  }
+ 
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;  
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    /* Initialization Error */
+    while(1); 
+  }  
+}
+#endif
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
