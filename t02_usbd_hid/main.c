bffb74938512d7b6bdea0b04abc031c4603f9ca5
BruceZ <xzhong86@163.com>
2020-03-08 19:56:01 +0800

    simply move mouse

--- t02_usbd_hid/main.c
+++ t02_usbd_hid/main.c
@@ -1,74 +1,18 @@
-/**
-  ******************************************************************************
-  * @file    USB_Device/HID_Standalone/Src/main.c
-  * @author  MCD Application Team
-  * @brief   USB device HID demo main file
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright © 2017 STMicroelectronics International N.V. 
-  * All rights reserved.</center></h2>
-  *
-  * Redistribution and use in source and binary forms, with or without 
-  * modification, are permitted, provided that the following conditions are met:
-  *
-  * 1. Redistribution of source code must retain the above copyright notice, 
-  *    this list of conditions and the following disclaimer.
-  * 2. Redistributions in binary form must reproduce the above copyright notice,
-  *    this list of conditions and the following disclaimer in the documentation
-  *    and/or other materials provided with the distribution.
-  * 3. Neither the name of STMicroelectronics nor the names of other 
-  *    contributors to this software may be used to endorse or promote products 
-  *    derived from this software without specific written permission.
-  * 4. This software, including modifications and/or derivative works of this 
-  *    software, must execute solely and exclusively on microcontroller or
-  *    microprocessor devices manufactured by or for STMicroelectronics.
-  * 5. Redistribution and use of this software other than as permitted under 
-  *    this license is void and will automatically terminate your rights under 
-  *    this license. 
-  *
-  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
-  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
-  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
-  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
-  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
-  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
-  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
-  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
-/* Includes ------------------------------------------------------------------ */
+
 #include "main.h"
 #include "msp_uart.h"
 #include "board_bsp.h"
 #include <stdio.h>
 
-/* Private typedef ----------------------------------------------------------- */
-/* Private define ------------------------------------------------------------ */
-#define CURSOR_STEP     5
 
-/* Private macro ------------------------------------------------------------- */
-/* Private variables --------------------------------------------------------- */
 USBD_HandleTypeDef USBD_Device;
 extern PCD_HandleTypeDef hpcd;
 
-/* Private function prototypes ----------------------------------------------- */
-
-/* Private functions --------------------------------------------------------- */
 int __io_putchar(int ch) { dap_putc_r(ch); }
 
-/**
-  * @brief  Main program
-  * @param  None
-  * @retval None
-  */
 int main(void)
 {
+  uint8_t HID_Buffer[4];
   /* STM32F412xx HAL library initialization */
   HAL_Init();
 
@@ -90,9 +34,15 @@ int main(void)
   USBD_Start(&USBD_Device); 
 
   printf("finish init. go into loop.\n");
+  HID_Buffer[0] = 0;
+  HID_Buffer[1] = 5;
+  HID_Buffer[2] = 5;
+  HID_Buffer[3] = 0;
   while (1)
   {
     HAL_Delay(100);
+
+    USBD_HID_SendReport(&USBD_Device, HID_Buffer, 4);
 
     /* Toggle LEDs */
     //BSP_LED_Toggle(LED1);
@@ -144,45 +94,12 @@ void SystemClock_Config(void)
 }
 
 
-/**
-  * @brief This function provides accurate delay (in milliseconds) based 
-  *        on SysTick counter flag.
-  * @note This function is declared as __weak to be overwritten in case of other
-  *       implementations in user file.
-  * @param Delay: specifies the delay time length, in milliseconds.
-  * @retval None
-  */
-
-void HAL_Delay(__IO uint32_t Delay)
-{
-  while (Delay)
-  {
-    if (SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk)
-    {
-      Delay--;
-    }
-  }
-}
-
 #ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
 void assert_failed(uint8_t * file, uint32_t line)
 {
-  /* User can add his own implementation to report the file name and line
-   * number, ex: printf("Wrong parameters value: file %s on line %d\r\n", file, 
-   * line) */
-
-  /* Infinite loop */
   while (1)
   {
   }
 }
 #endif
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
